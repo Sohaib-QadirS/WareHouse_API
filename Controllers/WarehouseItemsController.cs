cc32d9e0cd18bb15b69c2a7c939757568dfabe2e
Sohaib-QadirS <87055582+Sohaib-QadirS@users.noreply.github.com>
2021-10-13 08:23:53 +1300

    Add project files.

---
+++ Controllers/WarehouseItemsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WareHouse_API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WareHouse_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehouseItemsController : ControllerBase
+    {
+        // GET: api/WarehouseItems
+        [HttpGet]
+        public WarehouseItem Get()
+        {
+            WarehouseItem warehouseItem = new WarehouseItem();
+            return warehouseItem;
+
+        }
+        /* No need of overloading the GET or any other call types at this stage.*/
+
+        //// GET api/<WarehouseItemsController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+    }
+}
---
+++ Models/WarehouseItem.cs
@@ -0,0 +1,12 @@
+﻿namespace WareHouse_API.Models
+{
+    public class WarehouseItem
+    {   /*At this stage the model only uses strings to keep it simple otherwise currency can be decimal*/
+        public string TotalBins { get; set; } = "37608";
+        public string TotalVarities { get; set; } = "4";
+        public string TotalStaff { get; set; } = "157";
+        public string TotalWorkingHours { get; set; } = "5887090";
+        public string AverageRate { get; set; } = "23.50";
+        public string TotalLaborCost { get; set; } = "7829309";
+    }
+}
