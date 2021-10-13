79d251ce0bf69adb1ee5c1a77ff8ccc9072fa484
Sohaib-QadirS <87055582+Sohaib-QadirS@users.noreply.github.com>
2021-10-13 20:50:58 +1300

    Added Authorization

--- Controllers/WarehouseItemsController.cs
+++ Controllers/WarehouseItemsController.cs
@@ -1,20 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using WareHouse_API.Models;
+using WareHouse_API.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WareHouse_API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class WarehouseItemsController : ControllerBase
     {
+        private IUserService _userService;
+
+        public WarehouseItemsController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] AuthenticateModel model)
+        {
+            var user = await _userService.Authenticate(model.Username, model.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
         // GET: api/WarehouseItems
         [HttpGet]
-        public WarehouseItem Get()
+        public async Task<IActionResult> GetAll()
         {
+            var users = await _userService.GetAll();
             WarehouseItem warehouseItem = new WarehouseItem();
-            return warehouseItem;
+            return Ok(warehouseItem);
 
         }
         /* No need of overloading the GET or any other call types at this stage. */
--- Startup.cs
+++ Startup.cs
@@ -1,22 +1,36 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WareHouse_API.Helpers;
+using WareHouse_API.Services;
+using Microsoft.AspNetCore.Authentication;
 
 namespace WareHouse_API
 {
     public class Startup
     {
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
+
+            // configure basic authentication 
+            services.AddAuthentication("BasicAuthentication")
+                .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,18 +43,16 @@ namespace WareHouse_API
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                //endpoints.MapGet("/", async context =>
-                //{
-                //    await context.Response.WriteAsync("Hello World!");
-                //});
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                });
-            });
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
