428285c2b488f31298d3095c97c0f7b08c36e962
Sohaib-QadirS <87055582+Sohaib-QadirS@users.noreply.github.com>
2021-10-13 21:23:58 +1300

    Authentication and authorization added. User name: test Password: test

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -16,7 +16,7 @@ namespace WareHouse_API.Services
         // users hardcoded for simplicity, store in a db with hashed passwords in production applications
         private List<User> _users = new List<User>
         {
-            new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" }
+            new User { Id = 1, FirstName = "Sohaib", LastName = "Qadir", Username = "test", Password = "test" }
         };
 
         public async Task<User> Authenticate(string username, string password)
